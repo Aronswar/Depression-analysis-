19fea482f4f9e99ef01445a0469da448ddfbc3a4
Harun Guna <harung1993@gmail.com>
2016-12-04 17:05:37 -0500

    Update and rename test1.py.py to q_module.py
    Changed few parts need to create three more similar modules.

--- q_module.py
+++ q_module.py
@@ -9,9 +9,9 @@ def main():
                 5.Most of the Time""")
         action=input('1. Have you cried this week?:')
         fft=input("2.Do you feel that you are good at doing things?")
-        nxt=input('2. Have your been energy deprived for the past the past 7 days?:')
-        thrd=input("3.How was your sleep: ")
-        fur=input("4. How often have you felt sad in the past 7 days:")
+        nxt=input('3. Have your been energy deprived for the past the past 7 days?:')
+        thrd=input("4.How was your sleep for the past 7 days: ")
+        fur=input("5. How often have you felt sad in the past 7 days:")
 
         tot=action+fft+nxt+thrd+fur
         print tot
