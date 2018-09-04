4c367b4a06c773a3a922d2b46f3bd8a73959e951
Harun Guna <harung1993@gmail.com>
2018-09-04 00:11:23 -0400

    Update fb_module.py

--- fb_module.py
+++ fb_module.py
@@ -2,9 +2,7 @@ from facepy import GraphAPI
 import json
 import pickle
 
-access = 'EAACEdEose0cBALMlBlQKBpAPi9ERs8QRfXdTG747iP33cvuaBQYzKdMuhpG8h2FmUXZBCG04OCMuHHDZCkMwFkPerAnJdw9KZBumv67zD6zXegSZCU7ZBZBaef6RF9QN4WL7ndCH8XRWSRZBVsZBS7zKECAcYdcY544n3AonygsI7QZDZD'
-
-graph = GraphAPI(access)
+access = ''
 page_id= '821396057905476'
 datas= graph.get(page_id+'/posts?fields=message', page=True, retry=5)
 
